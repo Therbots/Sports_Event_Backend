cad8ed47f59a42cc378756386b36d6c2f19bcd83
taylor herbots <therbots@live.com>
2021-10-07 11:28:03 -0500

    crearted cars app + created car model + registered car model with admin site + migrated new car table

---
+++ sports_event_project/cars/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import User
+class Car(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    make = models.CharField(max_length=30)
+    model = models.CharField(max_length=100)
+    model = models.IntegerField(max_length=30)
+
+
+# Create your models here.
