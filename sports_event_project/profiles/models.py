829afd6b43bcbb8774ae92545cd6ccd58ba70d74
taylor herbots <therbots@live.com>
2021-10-11 19:38:00 -0500

    installed Pillows + added imagefield to profile model

--- sports_event_project/profiles/models.py
+++ sports_event_project/profiles/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+def upload_path(instance, filename):
+    return '/'.join(['image', str(instance.name), filename])
+
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=150)
+    image = models.ImageField(blank=True, null=True, upload_to=upload_path)
     street = models.CharField(max_length=150)
     city = models.CharField(max_length=150)
     state = models.CharField(max_length=100)
